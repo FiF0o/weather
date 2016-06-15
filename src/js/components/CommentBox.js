a4decdd4350a279c03c076b727e8f7bd08c8572c
jon <jonathan.lazarini@wipro.com>
2016-06-15 18:21:08 +0100

    add polling for AJAX request and memory leak fixing for the timer

--- src/js/components/CommentBox.js
+++ src/js/components/CommentBox.js
@@ -19,18 +19,39 @@ export default class CommentBox extends React.Component {
             comments: []
         };
 
-        //pre-bind method to class scope which is CommentBox and can be
-        // passed around without having to use .bind(this) at prop level in
-        // the component below onDelete={}
-        // bind(this) binds the context to the instance of the class rather
-        // than the class itself...
+        /*
+        pre-bind method to class scope which is CommentBox and can be
+        passed around without having to use .bind(this) at prop level in
+        the component below onDelete={}
+        bind(this) binds the context to the instance of the class rather
+        than the class itself...
+        */
         this._deleteComment = this._deleteComment.bind(this)
         this._addComment = this._addComment.bind(this)
     }
-    // lifecycle method is used as fetchComments calls render and it will be
-    // an infinite loop...
+    /*
+     lifecycle method is used as fetchComments calls render and it will be
+     an infinite loop...
+      */
+
     componentWillMount() {
         this._fetchComments();
+    }
+    /*
+     Start polling process for AJAX request here and rerenders components
+     every 5 secs
+     Memory leak as the timer is created (setInterval()) everytime the page is
+     rendered on
+      he page
+     */
+    
+    componentDidMount() {
+        this._timer = setInterval(() => this._fetchComments(), 5000)
+    }
+
+    //destroys timer when about to be remove removed - no more memory leak
+    componentWillUnmout() {
+        clearInterval(this_.timer);
     }
 
     render() {
